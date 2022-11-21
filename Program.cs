458a3f58c2a30c828a77db7bbbcffe04895a8a88
Olga Gorbunova <OBU@simcorp.com>
2022-11-21 13:58:08 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApplication
+{
+    class Program
+    {
+       static void Main(string[] args);
+
+        
+    }
+    
+    }
+
+// "Hello World" Programs
+namespace HelloWorld
+{ 
+    class Hello {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
